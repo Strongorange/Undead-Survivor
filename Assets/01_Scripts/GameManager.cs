e1a4843d030e2b8c4989e6539c6eecec11bc3aa7
Chanhwi Lee <chanhwi.lee6@gmail.com>
2024-07-03 22:49:49 +0900

    ✨ [FEAT] : 무한 맵 이동

---
+++ Assets/01_Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; // 정적으로 사용하겠다는 키워드. 바로 메모리에 얹어버림.
+    public PlayerController player;
+
+    void Awake()
+    {
+        instance = this;
+    }
+}
--- Assets/01_Scripts/PlayerController.cs
+++ Assets/01_Scripts/PlayerController.cs
@@ -6,12 +6,16 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
     public Vector2 inputVec;
     public float speed;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -23,5 +27,15 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if (inputVec.x != 0)
+        {
+            spriteRenderer.flipX = inputVec.x < 0;
+        }
     }
 }

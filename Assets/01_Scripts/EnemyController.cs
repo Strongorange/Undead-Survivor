0c80b135ed3329e951e47c89d1b8e802c83225c0
Chanhwi Lee <chanhwi.lee6@gmail.com>
2024-07-04 23:29:06 +0900

    ✨ [FEAT] : 적 플레이어 추적

---
+++ Assets/01_Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D target;
+    Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
+    public float speed;
+    bool isLive = true;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLive)
+        {
+            return;
+        }
+
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero; // 플레이어랑 충돌하면 충돌로 인해 속도가 생겨 이동 속도가 변하는 것을 방지
+    }
+
+    void LateUpdate()
+    {
+        if (!isLive)
+        {
+            return;
+        }
+        spriteRenderer.flipX = target.position.x < rigid.position.x;
+    }
+}
--- Assets/01_Scripts/RepositionController.cs
+++ Assets/01_Scripts/RepositionController.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class RepositionController : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     void OnTriggerExit2D(Collider2D other)
     {
         if (!other.CompareTag("Area"))
@@ -33,7 +40,13 @@ public class RepositionController : MonoBehaviour
                 }
                 break;
             case "Enemy":
-
+                if (coll.enabled)
+                {
+                    transform.Translate(
+                        playerDir * 20
+                            + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0)
+                    );
+                }
                 break;
         }
     }
